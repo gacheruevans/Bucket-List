3ff858b343282a6ed7273d1a53e2acdb01d02d14
Evans Gacheru <evans.gacheru@andela.com>
2017-01-07 20:24:35 +0300

    [chore]set up travis, tox, updates requiremenrs.txtand gitignore
    Signed-off-by: Evans Gacheru <evans.gacheru@andela.com>

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+   name='cp2-bucketlist',
+   description='Checkpoint 2',
+   author='Evans Gacheru',
+   author_email='evans.gacheru@andela.com',
+   url='https://github.com/andela-gacheruevans/cp2-bucketlist',
+   version='1.0.0'
+)
